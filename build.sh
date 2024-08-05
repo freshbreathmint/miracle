67c91b137aeec41c65fd6c67d9293d89fd1aeacd
mints <breathmints@protonmail.com>
2024-08-04 22:29:54 -0700

    Upgraded build script

--- build.sh
+++ build.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-echo "Running Build Script..."
+echo "Running build script..."
 
-# First Argument = Platform
-PLATFORM=$1
+# Variables
+ACTION=$1
+PLATFORM=$2
 
-# Create build directory if it doesn't exist.
-mkdir -p build/$PLATFORM
+# Function to clean build and bin directories
+clean() {
+    echo "Cleaning build and bin directories..."
+    rm -rf build bin
+    echo "Clean complete."
+}
 
-# Generate build files
-cmake -B build/$PLATFORM -DPLATFORM=$PLATFORM
+# Function to build for a specified platform
+make() {
+    # Check if platform variable was specified
+    if [ -z "$PLATFORM" ]; then
+        echo "Error: No platform specified. Please specify a platform such as [native|windows]."
+        exit 1
+    fi
+    
+    # Create build directory if it doesn't already exist
+    mkdir -p build/$PLATFORM
+    
+    # Generate build files & execute build
+    cmake -B build/$PLATFORM -DPLATFORM=$PLATFORM
+    cmake --build build/$PLATFORM
+    echo "Build for platform '$PLATFORM' complete."
+}
 
-# Build
-cmake --build build/$PLATFORM
+case $ACTION in
+    make)
+        make
+        ;;
+    clean)
+        clean
+        ;;
+    *)
+        echo "Invalid action. Use 'make' followed by a platform to build, or 'clean' to clean the build files."
+        exit 1
+        ;;
+esac
