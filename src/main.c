1594642689726b82b4ce7766d9b9b3100b3cfb4d
mints <breathmints@protonmail.com>
2024-08-11 22:05:33 -0700

    benchmarking stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("Hello World!\n");
-    getchar();
+    clock_t start_time, end_time;
+    double time_spent;
+
+    long long int num_iterations = 1000000000;
+    double x = 1.5;
+
+    int counting = 0;
+    while (counting <= 10000000000){
+        printf("%i\n", counting);
+        counting++;
+    }
+
+    start_time = clock();
+
+    // Calculate the floating point operations
+    for(long long int i = 0; i < num_iterations; i++){
+        x = x * 1.000001;
+        x = x / 1.000001;
+    }
+
+    end_time = clock();
+
+    time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("TIME: %f\n", time_spent);
+
+    sleep(5);
+
     return 0;
 }
