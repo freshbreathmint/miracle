d5d8ddffc49e4c662ffb31f57304ff9fbcb70165
mints <breathmints@protonmail.com>
2024-08-21 21:08:15 -0700

    src directory for application

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Making directories..."
+mkdir -p ../src
+
+echo "Copying Workspace file..."
 cp "application/miracle.code-workspace" "../"
 echo "Workspace file copied. Please open the workspace in Visual Studio Code."
