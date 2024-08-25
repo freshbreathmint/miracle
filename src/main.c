3208bc05c5ccf08e45d9498d98a763f862fbd32a
mints <breathmints@protonmail.com>
2024-08-25 00:12:19 -0700

    get absolute path of library and executable

--- src/main.c
+++ src/main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+//path stuff
+#include <unistd.h>
+#include <libgen.h>
+
 int main()
 {
+    // Get path of the executable
+    char exe_path[4096];
+    ssize_t len = readlink("/proc/self/exe", exe_path, sizeof(exe_path) - 1);
+    exe_path[len] = '\0'; // Null-terminate the path
+    printf("Executable Path: %s\n", exe_path);
+
+    // Get the directory
+    char *dir = dirname(exe_path);
+
+    // Construct the path to the shared library
+    char lib_path[4096];
+    snprintf(lib_path, sizeof(lib_path), "%s/libapplication.so", exe_path);
+    printf("Library Path: %s\n", lib_path);
+
     // Load Library
-    void *handle = dlopen("./bin/linux/debug/libapplication.so", RTLD_LAZY); 
-    //TODO: this path is for some reason relative to where the application has been LAUNCHED from
-    //      meaning that it's useless if you're not launching from the VSC run task. Alternatively
-    //      if I were to remove the bin/linux/debug/ portion of it, it would be runnable from the
-    //      actual directory without a problem. Not sure how to fix this at the moment
+    void *handle = dlopen(lib_path, RTLD_LAZY); 
     if (!handle) {
         fprintf(stderr, "Error: %s\n", dlerror());
         return 1;
