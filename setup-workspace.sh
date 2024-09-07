ce66b0e78be9570d3e0e428d13f1dd8933d770e3
mints <breathmints@protonmail.com>
2024-09-07 03:20:30 -0700

    add c_cpp_properties to setup

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -2,7 +2,11 @@
 
 echo "Making directories..."
 mkdir -p ../src
+mkdir -p ../include
+mkdir -p ../.vscode
 
-echo "Copying Workspace file..."
+echo "Copying files..."
 cp "application/miracle.code-workspace" "../"
-echo "Workspace file copied. Please open the workspace in Visual Studio Code."
+cp "application/.vscode/c_cpp_properties.json" "../.vscode/"
+
+echo "Files copied. Please open the workspace in Visual Studio Code."
