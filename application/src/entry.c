b388095e6b4385286a33178e2d48c7138d67bf8a
mints <breathmints@protonmail.com>
2024-09-17 02:53:49 -0700

    application setup script

---
+++ application/src/entry.c
@@ -0,0 +1,8 @@
+#include "entry.h"
+#include "libapi.h"
+
+#include <stdio.h>
+
+void entry(){
+    printf("Application says hello!\n");
+}
--- setup-workspace.sh
+++ setup-workspace.sh
@@ -8,5 +8,9 @@ mkdir -p ../.vscode
 echo "Copying files..."
 cp "application/miracle.code-workspace" "../"
 cp "application/.vscode/c_cpp_properties.json" "../.vscode/"
+cp "application/include/entry.h" "../include/"
+cp "application/include/libapi.h" "../include/"
+cp "application/include/libapi_dynamic.h" "../include/"
+cp "application/src/entry.c" "../src/"
 
 echo "Files copied. Please open the workspace in Visual Studio Code."
