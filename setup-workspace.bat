a3eb1c48b7baf5b6423bbc0f65dfc14e17f09d83
mints <breathmints@protonmail.com>
2024-09-22 02:24:40 -0700

    remove workspace bat file

--- setup-workspace.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Making directories...
-mkdir ..\src
-
-echo Copying Workspace file...
-copy "application\miracle.code-workspace" "..\"
-echo Workspace file copied. Please open the workspace in Visual Studio Code.
